c899ca32cc590182043c98b055c5bd8443e06647
Miguel Galindo <miguel.galindos@campusucc.edu.co>
2021-09-02 20:02:47 -0500

    01. mi primer comit en git para el programa

---
+++ my_program.cpp
@@ -0,0 +1,18 @@
+<studio.h>
+<math.h>
+<feth.h>
+
+#workspace 'curso devops'
+
+	int main[object ...] {
+	int invalor = 0;
+
+	cin << 'ingresar un valor para calcular una suma';
+
+	for (i=1; <= ivalor; i++) {
+	icontador+=i;
+	}
+	cout >> ' El resultado es: '+%int, icontador;
+
+	getch();
+	exit1;
