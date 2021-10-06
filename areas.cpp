0d0409d958b3ec744f7ea62183a57d85d894b8f7
Miguel Galindo <miguel.galindos@campusucc.edu.co>
2021-10-06 16:30:32 -0500

    Creamos un menu para seleccionar el tipo de area para calcular el tipo de area

--- areas.cpp
+++ areas.cpp
@@ -1,3 +1,15 @@
 # include <stdio.h>
 # include <iostream>
 using namespace std;
+
+int main ()
+{
+        int op;
+        cout<< "1. Calcular el area de un cuadrado" <<endl;
+        cout<< "2.Calcular el area de un triangulo" <<endl;
+        cout<< "3.Calcular el area de un circulo" <<endl;
+        cout<< endl ;
+        cout<< "Elija una opcion"<<endl;
+        cin>>   op ;
+        int lado,base,altura,radio;
+        switch(op)
