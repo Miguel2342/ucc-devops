5bdc44694bf172e4f956c0fe79605b8d52ab35c2
Miguel Galindo <miguel.galindos@campusucc.edu.co>
2021-10-14 19:41:08 -0500

    1.Update programs

---
+++ areasj.cpp
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include <iostream>
+using namespace std;
+int main()
+{{
+	int lado=5;
+	int base=9;
+	int altura=6;
+	int radio=8;                                        
+	cout <<"El area del cuadrado es:"<<lado*lado;
+	cout <<"\n El area del triangulo es:"<<(base*altura)/2;
+	cout <<"\n El area del circulo es:"<<3.1416*(radio*radio);
+}
+        cout <<"\n";
+        cout <<"+-----------------------------------------------------------------------+\n";
+        cout <<"|									|\n";
+        cout <<"|                                                                       |\n";
+        cout <<"|     	PROGRAMA PARA CALCULAR EL AREA DE UNA FIGURA GEOMETRICA		|\n";
+        cout <<"|         	   REALIZADO POR: MIGUEL GALINDO			|\n";
+        cout <<"|                                                                       |\n";
+        cout <<"|                                                                       |\n";
+        cout <<"+----------------------------------------------------------------------+\n";
+	cout <<"\n";
+return 0;
+}
