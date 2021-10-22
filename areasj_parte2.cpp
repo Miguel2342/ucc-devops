bd1d2481976e259964e8ea9a690447a7bb6ecc73
Miguel Galindo <miguel.galindos@campusucc.edu.co>
2021-10-21 20:34:43 -0500

    PARTICION DE areasj.cpp en areasj_parte1.cpp y areasj_parte2.cpp

---
+++ areasj_parte2.cpp
@@ -0,0 +1,8 @@
+
+	raiz = sqrt(vf_area);
+        cout<<"la raiz cuadra de "<< vf_area<<" es:" << raiz;
+
+	//Archivo Funcion
+	//a vf_area calcular raiz cuadrada en una variable e imprimir el resultado
+	return 0;
+}
